41a3be7aebb9b40cfa60069f7240c13a048b35e9
LeRoy Rosen <113536685+NordiskPirat@users.noreply.github.com>
2024-01-12 18:41:25 -0700

    first assignment done

--- cse341/controllers/index.js
+++ cse341/controllers/index.js
@@ -1,5 +1,5 @@
-const awesomeFunction = (req, res, next) => {
-  res.json("Awesome person");
+const personFunction = (req, res, next) => {
+  res.json("Candaith Rosen");
 };
 
-module.exports = { awesomeFunction };
+module.exports = { personFunction };
